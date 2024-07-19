b6ad817aef11c937df545809896c34f979cfb558
Курганов Никита <110167907+sooriken@users.noreply.github.com>
2024-07-19 17:16:37 +0300

    New blocks
    Добавлены новые блоки

--- app/src/js/main.js
+++ app/src/js/main.js
@@ -79,3 +79,21 @@ document.getElementById("sidenavClose").addEventListener('click',function (){
     document.getElementById("sidenavPlaceholder").classList.add("side-nav__placeholder--hidden");
 }); 
 
+// Скролл до элементов
+document.querySelectorAll("a[href^='#']").forEach(link => {
+    console.log('22');
+    link.addEventListener("click", function (e) {
+        e.preventDefault();
+        let href = this.getAttribute("href").substring(1);
+        const scrollTarget = document.getElementById(href);
+        const topOffset = 0;
+        // const topOffset = 0; // если не нужен отступ сверху
+        const elementPosition = scrollTarget.getBoundingClientRect().top;
+        const offsetPosition = elementPosition - topOffset;
+
+        window.scrollBy({
+            top: offsetPosition,
+            behavior: "smooth"
+        });
+    });
+});
